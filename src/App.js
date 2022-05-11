db7b10b7445402b9dd0f4c01ebd94ef7a230e250
Roman Ivaskevich <ivan432008@gmail.com>
2022-05-11 13:21:24 +0300

    react 18

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./components/Counter/Counter";
+
+const App = () => (
+  <>
+    <h1>Состояние компонента</h1>
+
+    <Counter />
+  </>
+);
+
+export default App;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Counter.css";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="Counter">
+        <span className="Counter__value">0</span>
+
+        <div className="Counter__controls">
+          <button type="button">Увеличить на 1</button>
+          <button type="button">Уменьшить на 1</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
